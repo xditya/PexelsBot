b982ec5fc9b5069ee030c8eae83a85e2c400f4d5
Aditya <xditya@ultroid.tech>
2022-09-28 18:52:47 +0530

    feat: non-spammy broadcast

--- src/modules/mod.ts
+++ src/modules/mod.ts
@@ -3,10 +3,10 @@ import { Composer } from "grammy/mod.ts";
 import start from "./start.ts";
 import inline from "./inline.ts";
 import stats from "./stats.ts";
-// import broadcast from "./broadcast.ts";
+import broadcast from "./broadcast.ts";
 
 const composer = new Composer();
 
-composer.use(start, inline, stats);
+composer.use(start, inline, stats, broadcast);
 
 export default composer;
